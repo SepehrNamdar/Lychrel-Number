7f1ad2e08adb74beabcc535d137135c12ef9ca50
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-08-12 10:52:05 +0200

    Solution step 2

--- src/main/java/kata/LychrelNumber.java
+++ src/main/java/kata/LychrelNumber.java
@@ -1,7 +1,33 @@
 package kata;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class LychrelNumber {
-    public boolean isLychrel(int number) {
-        return false;
+
+    private int limitIteration;
+
+    public LychrelNumber(int limitIteration) {
+        this.limitIteration = limitIteration;
+    }
+
+    public boolean isLychrel(BigInteger number) {
+        if (limitIteration == 0) return true;
+        if (isPalindrome(number)) return false;
+        BigInteger sum = number.add(reverse(number));
+        limitIteration--;
+        return isLychrel(sum);
+    }
+
+    private boolean isPalindrome(BigInteger number) {
+        BigInteger reversedNumber = reverse(number);
+        return reversedNumber.equals(number);
+    }
+
+    private BigInteger reverse(BigInteger number) {
+        String reversed = Arrays.stream(String.valueOf(number).split(""))
+                .reduce((x, y) -> y + x)
+                .orElseThrow();
+        return new BigInteger(reversed);
     }
 }
