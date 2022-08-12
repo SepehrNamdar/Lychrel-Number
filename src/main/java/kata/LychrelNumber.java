b165ef7fdf24c8fab7249c325349b14526fe4c84
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-08-12 10:22:05 +0200

    Solution step 1

--- src/main/java/kata/LychrelNumber.java
+++ src/main/java/kata/LychrelNumber.java
@@ -1,7 +1,25 @@
 package kata;
 
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
+
 public class LychrelNumber {
+
     public boolean isLychrel(int number) {
-        return false;
+        int reversed = reverse(number);
+        return !isPalindrome(reversed + number);
     }
+
+    private boolean isPalindrome(int number) {
+        int reversedNumber = reverse(number);
+        return reversedNumber == number;
+    }
+
+    private int reverse(int entry) {
+        String reversed = stream(String.valueOf(entry).split(""))
+                .reduce((x, y) -> y + x)
+                .orElseThrow();
+        return parseInt(reversed);
+    }
+
 }
